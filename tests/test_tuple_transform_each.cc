588147dd497d393ebeb8a5a1bc4f21b9aec66759
Murray Cumming <murrayc@murrayc.com>
2016-01-10 17:20:23 +0100

    Trying to add tuple_type_transform_each.
    g++ 5.2.1 says:
    
    ./tuple-utils/tuple_transform_each.h:39:98: error: template argument 1 is invalid
         typename std::result_of<T_transformer<from_element_type>::transform(const from_element_type&)>::type;
    
    clang++ 3.6 says:
    
    ./tuple-utils/tuple_transform_each.h:39:73: error: expected expression
        typename std::result_of<T_transformer<from_element_type>::transform(const from_element_type&)>::type;
                                                                            ^
    ./tuple-utils/tuple_transform_each.h:39:101: error: expected ';' after alias declaration
        typename std::result_of<T_transformer<from_element_type>::transform(const from_element_type&)>::type;

--- tests/test_tuple_transform_each.cc
+++ tests/test_tuple_transform_each.cc
@@ -18,11 +18,8 @@
 #include <utility>
 #include <cstdlib>
 #include <cassert>
-
-void test_tuple_type_transform_each()
-{
- //TODO
-}
+//#include <typeinfo>
+#include <iostream>
 
 template <class T_element_from>
 class transform_to_string
@@ -34,6 +31,24 @@ public:
     return std::to_string(from);
   } 
 };
+
+void test_tuple_type_transform_each_same_types()
+{
+  using type_tuple_original = std::tuple<int, int>;
+  using type_tuple_transformed = tupleutils::tuple_type_transform_each<
+    type_tuple_original, transform_to_string>::type;
+  using type_tuple_expected = std::tuple<std::string, std::string>;
+
+  //TODO: This is just here to make the compiler mention the actual tuple type
+  //when the tuple type is not as expected (type_tuple_expected). Remove this
+  //when we fix that problem.
+  type_tuple_transformed a("2", "3");
+  std::cout << std::get<0>(a) << std::endl;
+  //std::cout << typeid(type_tuple_transformed).name() << std::endl;
+
+  static_assert(std::is_same<type_tuple_transformed, type_tuple_expected>::value,
+    "unexpected tuple_transform_each()ed tuple type");
+}
 
 //In these tests, t_expected has elements all of the same type.
 void test_tuple_transform_each_same_types()
@@ -136,7 +151,7 @@ void test_tuple_transform_each_multiple_types()
 
 int main()
 {
-  test_tuple_type_transform_each();
+  test_tuple_type_transform_each_same_types();
 
   test_tuple_transform_each_same_types();
   test_tuple_transform_each_multiple_types();

b7a36e16cdb1aff23869b2c92c25442357357b1f
Murray Cumming <murrayc@murrayc.com>
2016-01-11 07:53:40 +0100

    Added tupleutils::tuple_for_each().
    This is like tuple_transform_each() but is much simpler, not
    returning any tuple. Like tuple_transform_each, it takes a template
    template parameter, so it can take any type, meaning that the
    tuple elements don't need to all be of the same type or derive from
    the same class.

---
+++ tests/test_tuple_for_each.cc
@@ -0,0 +1,103 @@
+/* Copyright (C) 2016 Murray Cumming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#include <tuple-utils/tuple_for_each.h>
+#include <utility>
+#include <cstdlib>
+#include <cassert>
+//#include <typeinfo>
+#include <iostream>
+
+template <class T_element_from>
+class for_each_simple
+{
+public:
+  static
+  void
+  visit(const T_element_from& from) {
+    std::cout << "for_each_simple(): " << std::to_string(from) << std::endl;
+  } 
+};
+
+void test_tuple_for_each_same_types()
+{
+  {
+    auto t_original = std::make_tuple(1, 2, 3);
+    tupleutils::tuple_for_each<for_each_simple>(t_original);
+  }
+
+  {
+    auto t_original = std::make_tuple(1, (double)2.1f, 3);
+    tupleutils::tuple_for_each<for_each_simple>(t_original);
+  }
+}
+
+
+//The general template declaration.
+//We then provide specializations for each type,
+//so we can test having a different return value for each T_element_from type.
+template <class T_element_from>
+class visitor_with_specializations;
+
+//An int will be converted to a std::string:
+template <>
+class visitor_with_specializations<int>
+{
+public:
+  static
+  void
+  visit(const int& from) {
+    std::cout << "visitor_with_specializations::visit(): " << std::to_string(from) << std::endl;
+  } 
+};
+
+//A double will be converted to a char:
+template <>
+class visitor_with_specializations<double>
+{
+public:
+  static
+  void
+  visit(const double& from) {
+    std::cout << "visitor_with_specializations::visit(): " << std::to_string(from)[0] << std::endl;
+  } 
+};
+
+//A std::string will be converted to an int:
+template <>
+class visitor_with_specializations<std::string>
+{
+public:
+  static
+  void
+  visit(const std::string& from) {
+    std::cout << "visitor_with_specializations::visit(): " << std::stoi(from) << std::endl;
+  } 
+};
+
+void test_tuple_for_each_multiple_types()
+{
+  auto t_original = std::make_tuple(1, (double)2.1f, std::string("3"));
+  tupleutils::tuple_for_each<visitor_with_specializations>(t_original);
+}
+
+int main()
+{
+  test_tuple_for_each_same_types();
+  test_tuple_for_each_multiple_types();
+      
+  return EXIT_SUCCESS;
+}
---
+++ tuple-utils/tuple_for_each.h
@@ -0,0 +1,78 @@
+/* Copyright (C) 2016 Murray Cumming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#ifndef _MURRAYC_TUPLE_UTILS_TUPLE_FOR_EACH_H_
+#define _MURRAYC_TUPLE_UTILS_TUPLE_FOR_EACH_H_
+
+#include <tuple-utils/tuple_cat.h>
+#include <tuple-utils/tuple_cdr.h>
+#include <tuple-utils/tuple_start.h>
+#include <tuple-utils/tuple_end.h>
+#include <type_traits>
+
+namespace tupleutils
+{
+
+namespace {
+
+template<template<typename> class T_visitor, std::size_t index>
+struct tuple_for_each_impl
+{
+  template<typename T>
+  static
+  void
+  tuple_for_each(const T& t) {
+    using element_type = typename std::tuple_element<index, T>::type;
+    T_visitor<element_type>::visit(std::get<index>(t));
+
+    tuple_for_each_impl<T_visitor, index - 1>::tuple_for_each(t);
+  }
+};
+
+template<template<typename> class T_visitor>
+struct tuple_for_each_impl<T_visitor, 0>
+{
+  template<typename T>
+  static
+  void
+  tuple_for_each(const T& t) {
+    constexpr std::size_t index = 0;
+
+    using element_type = typename std::tuple_element<index, T>::type;
+    T_visitor<element_type>::visit(std::get<index>(t));
+  }
+};
+
+} //anonymous namespace
+
+/**
+ * Get a tuple with each element having the transformed value of the element
+ * in the original tuple.
+ *
+ * @tparam T_visitor should be a template that has a static visit() method.
+ * @tparam T the tuple type.
+ * @param t The tuple whose elements should be visited.
+ */
+template<template<typename> class T_visitor, typename T>
+void
+tuple_for_each(const T& t) {
+  constexpr auto size = std::tuple_size<T>::value; 
+  tuple_for_each_impl<T_visitor, size - 1>::tuple_for_each(t);
+}
+
+} //namespace tupleutils
+
+#endif //_MURRAYC_TUPLE_UTILS_TUPLE_FOR_EACH_H_

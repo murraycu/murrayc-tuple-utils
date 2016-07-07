5081a1d95d146b661657c94b2e0bf46f8e456872
Murray Cumming <murrayc@murrayc.com>
2016-07-07 21:59:07 +0200

    C++17: tuple_transform_each(): Use constexpr if instead of specializations

--- tuple-utils/tuple_transform_each.h
+++ tuple-utils/tuple_transform_each.h
@@ -95,69 +95,40 @@ struct tuple_transform_each_impl {
   constexpr
   static decltype(auto)
   tuple_transform_each(T_current&& t, T_original& t_original) {
-    //We use std::decay_t<> because tuple_size is not defined for references.
-    constexpr auto size = std::tuple_size<std::decay_t<T_current>>::value;
-    static_assert(size > 1, "size must be more than 0.");
-
-    constexpr auto index = size - size_from_index;
-    static_assert(index >= 0, "unexpected index.");
-
-    using from_element_type = typename std::tuple_element<index, std::decay_t<T_original>>::type;
-    using to_element_type = typename std::result_of<decltype (
-      &T_transformer<from_element_type>::transform)(from_element_type&)>::type;
-    const auto t_element =
-      std::tuple<to_element_type>(T_transformer<from_element_type>::transform(std::get<index>(t_original)));
-
-    const auto t_start = tuple_start<index>(std::forward<T_current>(t));
-
-    // t_end's elements will be copies of the elements in t, so this method's
-    // caller won't see the changes made in the subsequent call of
-    // tuple_transform_each() on those copies. That's why we pass t_original
-    // through too, so we can modify that directly.
-    // the const version (tuple_transform_each_const()) doesn't have to worry
-    // about this, though avoiding copying would be more efficient.
-    const auto t_end = tuple_end<size - index - 1>(t);
-
-    auto t_with_transformed_element = std::tuple_cat(t_start, t_element, t_end);
-    return tuple_transform_each_impl<T_transformer,
-      size_from_index - 1>::tuple_transform_each(t_with_transformed_element, t_original);
-  }
-};
-
-template <template <typename> class T_transformer>
-struct tuple_transform_each_impl<T_transformer, 1> {
-  template <typename T_current, typename T_original>
-  constexpr
-  static
-  decltype(auto)
-  tuple_transform_each(T_current&& t, T_original& t_original) {
-    //We use std::decay_t<> because tuple_size is not defined for references.
-    constexpr auto size = std::tuple_size<std::decay_t<T_current>>::value;
-    static_assert(size > 0, "size must be more than 0.");
-
-    constexpr auto index = size - 1;
-    static_assert(index >= 0, "unexpected index.");
-
-    using from_element_type = typename std::tuple_element<index, T_original>::type;
-    using to_element_type = typename std::result_of<decltype (
-      &T_transformer<from_element_type>::transform)(from_element_type&)>::type;
-    const auto tuple_element =
-      std::tuple<to_element_type>(T_transformer<from_element_type>::transform(std::get<index>(t_original)));
-
-    const auto tuple_rest = tuple_start<size - 1>(std::forward<T_current>(t));
-    return std::tuple_cat(tuple_rest, tuple_element);
-  }
-};
-
-template <template <typename> class T_transformer>
-struct tuple_transform_each_impl<T_transformer, 0> {
-  template <typename T_current, typename T_original>
-  constexpr
-  static
-  decltype(auto)
-  tuple_transform_each(T_current&& t, T_original& /* t_original */) {
+    if constexpr(size_from_index == 0) {
       //Do nothing because the tuple has no elements.
       return std::forward<T_current>(t);
+    } else { //TODO: Should this compile without using else to contain the alternative code?
+      //We use std::decay_t<> because tuple_size is not defined for references.
+      constexpr auto size = std::tuple_size<std::decay_t<T_current>>::value;
+      constexpr auto index = size - size_from_index;
+      static_assert(index >= 0, "unexpected index.");
+
+      using from_element_type = typename std::tuple_element<index, std::decay_t<T_original>>::type;
+      using to_element_type = typename std::result_of<decltype (
+        &T_transformer<from_element_type>::transform)(from_element_type&)>::type;
+      const auto t_element =
+        std::tuple<to_element_type>(T_transformer<from_element_type>::transform(std::get<index>(t_original)));
+
+      if constexpr(size == 1) {
+        const auto tuple_rest = tuple_start<size - 1>(std::forward<T_current>(t));
+        return std::tuple_cat(tuple_rest, t_element);
+      } else {
+        const auto t_start = tuple_start<index>(std::forward<T_current>(t));
+
+        // t_end's elements will be copies of the elements in t, so this method's
+        // caller won't see the changes made in the subsequent call of
+        // tuple_transform_each() on those copies. That's why we pass t_original
+        // through too, so we can modify that directly.
+        // the const version (tuple_transform_each_const()) doesn't have to worry
+        // about this, though avoiding copying would be more efficient.
+        const auto t_end = tuple_end<size - index - 1>(t);
+
+        auto t_with_transformed_element = std::tuple_cat(t_start, t_element, t_end);
+        return tuple_transform_each_impl<T_transformer,
+          size_from_index - 1>::tuple_transform_each(t_with_transformed_element, t_original);
+      }
+    }
   }
 };
 
